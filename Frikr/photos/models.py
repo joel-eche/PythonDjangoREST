5b674e62df15f87c7bca79baf7f1b2377a0dbcb7
Joel Eche <joel.eche5@hotmail.com>
2016-11-23 18:01:31 -0500

    Admin de Django, fin tutorial

--- Frikr/photos/models.py
+++ Frikr/photos/models.py
@@ -18,3 +18,6 @@ class Photo(models.Model):
     created_at = models.DateTimeField(auto_now_add=True)
     modified_at = models.DateTimeField(auto_now=True)
     license = models.CharField(max_length=3,choices=LICENSES)
+
+    def __unicode__(self):
+        return self.name
