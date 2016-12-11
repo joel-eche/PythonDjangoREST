3f88fed8618ee9213e603490a69100d77792908e
Joel Eche <joel.eche5@gmail.com>
2016-12-11 15:47:45 -0500

    modificación del modelo

--- Frikr/photos/models.py
+++ Frikr/photos/models.py
@@ -1,4 +1,8 @@
+# -*- coding: utf-8 -*-
+
 from __future__ import unicode_literals
+
+from django.contrib.auth.models import User
 from django.db import models
 
 COPYRIGHT='RIG'
@@ -11,13 +15,24 @@ LICENSES=(
     (CREATIVE_COMMONS,'Creative Commons')
 )
 
+PUBLIC = 'PUB'
+PRIVATE= 'PRI'
+
+VISIBILITY=(
+    (PUBLIC,'Pública'),
+    (PRIVATE,'Privada')
+)
+
 class Photo(models.Model):
+
+    owner=models.ForeignKey(User)
     name = models.CharField(max_length=150)
     url = models.URLField()
     description = models.TextField(blank=True,null=True,default="")
     created_at = models.DateTimeField(auto_now_add=True)
     modified_at = models.DateTimeField(auto_now=True)
     license = models.CharField(max_length=3,choices=LICENSES)
+    visibility=models.CharField(max_length=3,choices=VISIBILITY,default=PUBLIC)
 
     def __unicode__(self):
         return self.name
