4fccefd43300ab81687d6c599f5175b11f6f0d8a
Joel Eche <joel.eche5@gmail.com>
2016-12-14 20:59:30 -0500

    static

--- Frikr/frikr/settings.py
+++ Frikr/frikr/settings.py
@@ -128,6 +128,7 @@ USE_TZ = True
 
 STATIC_URL = '/static/'
 
+
 #Logs all SQL queries in debug mode
 LOGGING={
     'version':1,
@@ -153,4 +154,11 @@ LOGIN_URL='/login' #cuando un usuario no está autenticado, le redirige a este e
 LICENSES=(
     ('QUE','Quentin Tarantino'),
     ('DSH','Dr. Schutlz')
+)
+
+STATICFILES_DIRS = (
+    # Put strings here, like "/home/html/static" or "C:/www/django/static".
+    # Always use forward slashes, even on Windows.
+    # Don't forget to use absolute paths, not relative paths.
+    os.path.join(BASE_DIR,'static'),
 )
