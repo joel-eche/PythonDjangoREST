84daac1412187b30ac73edc250ec3d24ea316be3
Joel Eche <joel.eche5@gmail.com>
2016-12-18 03:47:31 -0500

    Listado de fotos con ModelSerializer

---
+++ Frikr/photos/api.py
@@ -0,0 +1,14 @@
+# -*- coging:utf-8 -*-
+from rest_framework.response import Response
+from rest_framework.views import APIView
+
+from photos.models import Photo
+from photos.serializers import PhotoSerializer
+
+
+class PhotoListAPI(APIView):
+    def get(self,request):
+        photos=Photo.objects.all()
+        serializer=PhotoSerializer(photos,many=True)
+        return Response(serializer.data)
+
---
+++ Frikr/photos/serializers.py
@@ -0,0 +1,10 @@
+# -*- coging:utf-8 -*-
+from rest_framework import serializers
+
+from photos.models import Photo
+
+
+class PhotoSerializer(serializers.ModelSerializer):
+    class Meta:
+        model=Photo
+        fields = '__all__'
