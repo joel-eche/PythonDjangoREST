ee5079e7effe61d9938d0261836e614286ce9cc1
Joel Eche <joel.eche5@gmail.com>
2016-12-19 04:10:59 -0500

    Mostrar diferentes datos en List y Detail Photos REST

--- Frikr/photos/serializers.py
+++ Frikr/photos/serializers.py
@@ -8,3 +8,7 @@ class PhotoSerializer(serializers.ModelSerializer):
     class Meta:
         model=Photo
         fields = '__all__'
+
+class PhotoListSerializer(PhotoSerializer):
+    class Meta(PhotoSerializer.Meta):
+        fields = ('id','name','url')
