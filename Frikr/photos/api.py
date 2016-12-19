6c1dc32fd479974709c152136d09395c3fced2e9
Joel Eche <joel.eche5@gmail.com>
2016-12-19 18:42:15 -0500

    Protección de API fotos con clases de permisos genéricas

--- Frikr/photos/api.py
+++ Frikr/photos/api.py
@@ -1,5 +1,6 @@
 # -*- coging:utf-8 -*-
 from rest_framework.generics import ListCreateAPIView, RetrieveUpdateDestroyAPIView
+from rest_framework.permissions import IsAuthenticatedOrReadOnly
 
 from photos.models import Photo
 from photos.serializers import PhotoSerializer, PhotoListSerializer
@@ -7,6 +8,8 @@ from photos.serializers import PhotoSerializer, PhotoListSerializer
 
 class PhotoListAPI(ListCreateAPIView):
     queryset=Photo.objects.all()
+    permission_classes = (IsAuthenticatedOrReadOnly,)
+
     #serializer_class = PhotoListSerializer
     def get_serializer_class(self):
         return PhotoSerializer if self.request.method=="POST" else PhotoListSerializer
@@ -14,3 +17,4 @@ class PhotoListAPI(ListCreateAPIView):
 class PhotoDetailAPI(RetrieveUpdateDestroyAPIView):
     queryset = Photo.objects.all()
     serializer_class = PhotoSerializer
+    permission_classes = (IsAuthenticatedOrReadOnly,)
