76777bc8d6af60e3acdb056641724ed4f67b7171
Joel Eche <joel.eche5@gmail.com>
2016-12-18 23:53:31 -0500

    listado,creación,detalle,actualización y borrado de fotos API

--- Frikr/photos/api.py
+++ Frikr/photos/api.py
@@ -1,14 +1,13 @@
 # -*- coging:utf-8 -*-
-from rest_framework.response import Response
-from rest_framework.views import APIView
+from rest_framework.generics import ListCreateAPIView, RetrieveUpdateDestroyAPIView
 
 from photos.models import Photo
 from photos.serializers import PhotoSerializer
 
+class PhotoListAPI(ListCreateAPIView):
+    queryset=Photo.objects.all()
+    serializer_class = PhotoSerializer
 
-class PhotoListAPI(APIView):
-    def get(self,request):
-        photos=Photo.objects.all()
-        serializer=PhotoSerializer(photos,many=True)
-        return Response(serializer.data)
-
+class PhotoDetailAPI(RetrieveUpdateDestroyAPIView):
+    queryset = Photo.objects.all()
+    serializer_class = PhotoSerializer
